0d72f8f048aafaf0e0ec5f420cc0258e58fce334
Acosta, Ryan <Ryan.Acosta@sabre.com>
2017-03-10 14:26:40 -0600

    Completed problem 11

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,48 @@
 public class Main {
 
-    public static void main(String[] args) {
-        try {
-            if (args.length == 2) {
-                switch (Integer.parseInt(args[0]))
-                {
-                    case 1:
-                        new ProjectEulerProblem1(Integer.parseInt(args[1]));
-                        break;
-                    case 2:
-                        new ProjectEulerProblem2(Integer.parseInt(args[1]));
-                        break;
-                    case 3:
-                        new ProjectEulerProblem3(Long.parseLong(args[1]));
-                        break;
-                    case 4:
-                        new ProjectEulerProblem4();
-                        break;
-                    case 5:
-                        new ProjectEulerProblem5(Integer.parseInt(args[1]));
-                        break;
-                    case 6:
-                        new ProjectEulerProblem6(Integer.parseInt(args[1]));
-                        break;
-                    case 7:
-                        new ProjectEulerProblem7(Integer.parseInt(args[1]));
-                        break;
-                    case 8:
-                        new ProjectEulerProblem8();
-                        break;
-                    case 9:
-                        new ProjectEulerProblem9();
-                        break;
-                    case 10:
-                        new ProjectEulerProblem10(Long.parseLong(args[1]));
-                        break;
-                    default:
-                        System.err.println("Project Euler problem # " + args[0] + "is not supported");
-                }
+   public static void main(String[] args) {
+      try {
+         if (args.length == 2) {
+            switch (Integer.parseInt(args[0])) {
+               case 1:
+                  new ProjectEulerProblem1(Integer.parseInt(args[1]));
+                  break;
+               case 2:
+                  new ProjectEulerProblem2(Integer.parseInt(args[1]));
+                  break;
+               case 3:
+                  new ProjectEulerProblem3(Long.parseLong(args[1]));
+                  break;
+               case 4:
+                  new ProjectEulerProblem4();
+                  break;
+               case 5:
+                  new ProjectEulerProblem5(Integer.parseInt(args[1]));
+                  break;
+               case 6:
+                  new ProjectEulerProblem6(Integer.parseInt(args[1]));
+                  break;
+               case 7:
+                  new ProjectEulerProblem7(Integer.parseInt(args[1]));
+                  break;
+               case 8:
+                  new ProjectEulerProblem8();
+                  break;
+               case 9:
+                  new ProjectEulerProblem9();
+                  break;
+               case 10:
+                  new ProjectEulerProblem10(Long.parseLong(args[1]));
+                  break;
+               case 11:
+                  new ProjectEulerProblem11();
+                  break;
+               default:
+                  System.err.println("Project Euler problem # " + args[0] + "is not supported");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
 }
